c798de56b3dbc992c419c82e6a3db94db6e52442
Keys4Ever <cubillagonzalo@outlook.com>
2024-07-31 02:24:05 -0300

    Changed enviroment variables and moved them to .env using dotenv

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,1 @@
-/// <reference path="../.astro/db-types.d.ts" />
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-    readonly SUPABASE_URL: string;
-    readonly SUPABASE_AUTH_TOKEN: string;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
-  
--- src/lib/SupabaseClient.ts
+++ src/lib/SupabaseClient.ts
@@ -1,12 +1,14 @@
 import { createClient } from "@libsql/client";
+import { config } from "dotenv";
 
-// Usar variables de entorno de Vite (necesitan el prefijo `PUBLIC_` para el cliente)
-const databaseUrl = "libsql://saving-storm-cositoreal.turso.io";
-const autHToken = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjE2ODEzMzQsImlkIjoiYTBiOTVhZjUtNmUyMC00OTI5LWE1MDItNjhmZWJlMzEzM2E4In0.F_rkK0JCnOHbHS8x9ZRMIIjnC-_bSfNuBH-f3k6t3oF1-l-jYDJbf_f9_jHi3UXpKYpgzFB2O5qLPJnxAqbWCg";
+// Cargar variables de entorno desde el archivo .env
+config();
+
+// Usar variables de entorno
+const databaseUrl: string = process.env.DATABASE_URL as string;
+const authToken: string = process.env.AUTH_TOKEN as string;
 
 export const turso = createClient({
   url: databaseUrl,
-  authToken: autHToken
-}
-  
-);
+  authToken: authToken
+});
