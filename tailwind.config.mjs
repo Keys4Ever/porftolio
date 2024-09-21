9ef594bdd85315bcd4735cab533d51aa8c4500dc
Keys4Ever <cubillagonzalo@outlook.com>
2024-09-20 23:44:49 -0300

    Refactor: Rehice los colores, porque en su momento no sabía aprovechar las funciones de tailwind.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','tailwind.config.{js,cjs,mjs,ts}'],
 	theme: {
-	  extend: {},
+	  extend: {
+		colors:{
+			"card":{
+				"border":"#1f2d40",
+				"background": "#1f2d4099",
+			},
+			"button":{
+				"background":"#1f2d40",
+				"highlight":"#2d415e"
+			},
+			"text":{
+				"muted":"#bcc7d3",
+				"primary":"#f8ee33",
+				"secondary":"#fdfdc2"
+			},
+			"nav":"#1f2d40e6",
+			"tech":"#2e35411a"
+		}
+	  },
 	},
 	plugins: [],
   };
